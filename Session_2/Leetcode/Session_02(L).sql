377602901ed052da210510f182cdfebc1ecf48ce
Ashutosh Raj <ashuyadav77562@gmail.com>
2025-12-11 21:21:37 +0530

    Session2_Uploaded

---
+++ Session_2/Leetcode/Session_02(L).sql
@@ -0,0 +1,15 @@
+#G5_23BCS10514_AshutoshRaj_Session1
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    WITH
+      RankedEmployees AS (
+        SELECT *, DENSE_RANK() OVER(ORDER BY salary DESC) AS `rank`
+        FROM Employee
+      )
+    SELECT MAX(salary) AS SecondHighestSalary
+    FROM RankedEmployees
+    WHERE `rank` = N
+  );
+END
